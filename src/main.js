3239e0000e4a74c1c71a92fe1906691652baa9ef
Caio Henrique <caio.h.n.f@gmail.com>
2023-03-07 22:22:06 -0300

    finalização

--- src/main.js
+++ src/main.js
@@ -1,67 +1,67 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
 
-    const heroSection = document.querySelector('hero');
+    const heroSection = document.querySelector('.hero');
     const alturaHero = heroSection.clientHeight;
-    
-    window.addEventListener('scroll',function(){
+
+    window.addEventListener('scroll', function() {
         const posicaoAtual = window.scrollY;
 
-        if(posicaoAtual < alturaHero){
-            ocultaElementosdoHeader();
-        }else{
-            exibeElementosdoHeader();
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
         }
     })
 
-// Seção de atrações, programação das abas
-    for (let i = 0; i < buttons.length; i++){
-        buttons[i].addEventListener('click',function(botao){
+    // Seção de atrações, programação das abas
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
-            escodeTodasAbas();
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
 
-//seÇão FAQ, accordion
-    for (let i = 0; i < questions.length; i++){
+    // Seção FAQ, accordion
+    for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
 })
 
-function ocultaElementosdoHeader(){
+function ocultaElementosDoHeader() {
     const header = document.querySelector('header');
     header.classList.add('header--is-hidden');
 }
 
-function exibeElementosdoHeader(){
+function exibeElementosDoHeader() {
     const header = document.querySelector('header');
     header.classList.remove('header--is-hidden');
 }
 
-function abreOuFechaResposta(elemento){
+function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
     const elementoPai = elemento.target.parentNode;
 
     elementoPai.classList.toggle(classe);
 }
 
-function removeBotaoAtivo(){
+function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
-    for (let i = 0; i < buttons.length; i++){
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 }
 
-function escodeTodasAbas(){
+function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
-    for(let i = 0; i < tabsContainer.length; i++){
+    for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
 }
